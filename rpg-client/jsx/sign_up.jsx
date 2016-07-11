b1cc0120f62193d629b04a2506e226da9ce1e588
localadmin <localadmin@compozed-tss-13.mobile.asu.edu>
2016-07-11 15:55:48 -0700

    finish ui elements

--- rpg-client/jsx/sign_up.jsx
+++ rpg-client/jsx/sign_up.jsx
@@ -4,7 +4,11 @@ var Sign_up = React.createClass({
       <div>
         <field>
           <legend>
+            <label>Username:</label>
+            <br/>
             <input type='text' id='username_input'/>
+            <br/>
+            <label>Password:</label>
             <br/>
             <input type='text' id='password_input'/>
             <br/>
