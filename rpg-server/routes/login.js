6a985e7c5ab043a7c5992783033137ca65b2c760
localadmin <localadmin@compozed-tss-15.mobile.asu.edu>
2016-07-11 15:12:59 -0700

    Corrected sign in server stuff

--- rpg-server/routes/login.js
+++ rpg-server/routes/login.js
@@ -3,16 +3,19 @@ var router = express.Router();
 var db = require('../config/database');
 
 /* GET Login page. */
-router.get('/', function(req, res, next) {
-  res.json();
-});
-
-/* POST Login page. */
-router.post('/', function(req, res, next){
-  db.get('users').insert(req.body, function(err, data){
-    res.send('inside insert');
+router.get('/', function(req, res, next){
+  db.get('users').find(req.body, function(err, data){
+    if(data.length != 0){
+      res.json({LoggedIn: true});
+      return;
+    }
+    else res.json({LoggedIn: false});
   });
 });
+
+router.post('/', function(req, res, next){
+  res.json();
+})
 
 
 module.exports = router;
--- rpg-server/test/integration_test/sign_in_test.js
+++ rpg-server/test/integration_test/sign_in_test.js
@@ -5,20 +5,29 @@ var app = require('../../app');
 
 
 //GET
-describe('GET /', function(){
-  it('responds with a 200 code', function(done) {
-    request(app).get('/').expect(200, done);
+describe('GET /login', function(){
+  beforeEach(function (done) {
+    db.get('users').remove({}, function (err) {
+      if (err) done(err);
+      done();
+    });
+  });
+
+  it('responds with a 200 code: found', function(done) {
+    request(app).get('/login').expect(200, done);
   });
   it('responds with a 200 code given existing user in db', function(done){
     var user = {
       name: "Jay",
       password: "123"
     };
-    request(app).post('/').send(user).then(function(){
-    db.get('users').find({}, function (err, data) {
-      expect(data[0].name).to.equal('Jay')
-      done();
-  });
+    request(app).post('/login').send(user).then(function(){
+    db.get('users').insert(user).then(function(){
+      db.get('users').find({}, function (err, data) {
+        expect(data[0].name).to.equal('Jay')
+        done();
+    });
+    })
   });
   })
 });
