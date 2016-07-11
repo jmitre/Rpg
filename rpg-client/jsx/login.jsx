229f78a0e828f899991fcac187f3f37bd581149a
localadmin <localadmin@compozed-tss-15.mobile.asu.edu>
2016-07-11 15:47:29 -0700

    add dem files

--- rpg-client/jsx/login.jsx
+++ rpg-client/jsx/login.jsx
@@ -2,12 +2,11 @@ var Sign_in = React.createClass({
   render: function(){
     return(
       <div>
-        <h1>Log In Page
-        </h1>
         <field>
           <legend>
-            <input type='text' id='username_input'>
-            <input type='text' id='password_input'>
+            <input type='text' id='username_input'/>
+            <br/>
+            <input type='text' id='password_input'/>
           </legend>
         </field>
       </div>
@@ -15,4 +14,4 @@ var Sign_in = React.createClass({
   }
 });
 
-ReactDOM.render(<Sign_in/>, document.getElementById('sign-up-field'))
+ReactDOM.render(<Sign_in/>, document.getElementById('sign-in-field'))
