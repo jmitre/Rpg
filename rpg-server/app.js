5b3d06859cfe9eb8581c72c0b9773965260ecc28
localadmin <localadmin@compozed-tss-13.mobile.asu.edu>
2016-07-11 13:16:43 -0700

    Corrected app

--- rpg-server/app.js
+++ rpg-server/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var sign_up = require('./routes/signUp');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/sign-up',sign_up);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- rpg-server/test/integration_test/sign_up_test.js
+++ rpg-server/test/integration_test/sign_up_test.js
@@ -1,10 +1,21 @@
 var request = require('supertest');
 var expect = require('chai').expect
 var db = require('../../config/database');
+var app = require('../../app');
 
 describe('POST /sign-up', function(){
   it('responds with a 200 status code', function (done) {
         request(app).post('/sign-up')
           .expect(200, done)
     });
+  // it('given a user name and password, creates an account', function(){
+  //   var user = {
+  //     name: "Jay",
+  //     password: "123"
+  //   };
+  //
+  //   request(app).post('/sign-up').send(user).then(function(){
+  //     db.get('account')
+  //   });
+  // });
 });
