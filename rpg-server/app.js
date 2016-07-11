19c88e61d5ac138e5c0654f52e2e66398fba8d44
localadmin <localadmin@compozed-tss-13.mobile.asu.edu>
2016-07-11 16:46:42 -0700

    add cors

--- rpg-server/app.js
+++ rpg-server/app.js
@@ -4,11 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors')
 
 // var routes = require('./routes/index');
 var users = require('./routes/users');
 var sign_up = require('./routes/signUp');
 var login = require('./routes/login');
+
+
 
 
 var app = express();
@@ -19,6 +22,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
