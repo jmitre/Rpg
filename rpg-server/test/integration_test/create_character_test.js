df36c08190ee7f9afb48cf88bb8024c976311912
localadmin <localadmin@compozed-tss-15.mobile.asu.edu>
2016-07-13 11:55:55 -0700

    create character api started

--- rpg-server/test/integration_test/create_character_test.js
+++ rpg-server/test/integration_test/create_character_test.js
@@ -16,4 +16,31 @@ describe('POST /create-character', function(){
   it('responds with a 200 code: found', function(done) {
     request(app).post('/createCharacter').expect(200, done);
   });
+
+  xit('given an id and character object, it appends the user object in the database', function(done){
+    var user = {
+      name: 'Jay',
+      password: 'password'
+    };
+
+    var character = {
+      clas: 'Mage',
+      level: 1,
+      xp: 0
+    };
+
+
+    db.get('users').insert(user, function(err, data){
+      var message = {
+        id: data._id,
+        character: character
+      }
+      request(app).post(createCharacter).send(message).then(function(result){
+        db.get('users').find({}, function(err, data){
+          expect(data.character.clas).to.equal('Mage');
+          done();
+        });
+      });
+    });
+  });
 });
